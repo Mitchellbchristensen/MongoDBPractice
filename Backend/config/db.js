f902e1b6002d1290a485bd563c59a23b814221cd
Mitchell Christensen <mchristensen@verisk.com>
2022-07-21 14:31:20 -0600

    Commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbConnectionURI = `mongodb://claimx_automation-reports_test:KxR65nXO0q4EjZEJ@pl-0-us-west-2.vh4xo.mongodb.net:1024,pl-0-us-west-2.vh4xo.mongodb.net:1025,pl-0-us-west-2.vh4xo.mongodb.net:1026/claimx_automation-reports_test?ssl=true&replicaSet=atlas-gikv6i-shard-0&authSource=admin&retryWrites=true&w=majority`
+const dbConnectionURI = 'uri'
 
 const connectDB = async () => {
     try {
